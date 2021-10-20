8e773cde869d3802381a9d0f2329805f3efe2c4f
gczxtest <92834797+gczxtest@users.noreply.github.com>
2021-10-20 09:38:45 +0800

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,31 @@
+function getTemplateData(){
+    return data;
+}
+
+function isWebpSupported(){
+    return supportWebp;
+}
+
+var supportWebp = false
+check_webp_feature('lossy', function (feature, isSupported) {
+        supportWebp  = isSupported;
+        console.log('supportWebp = '+supportWebp);
+});
+
+function check_webp_feature(feature, callback) {
+    var kTestImages = {
+        lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
+        lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
+        alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
+        animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
+    };
+    var img = new Image();
+    img.onload = function () {
+        var result = (img.width > 0) && (img.height > 0);
+        callback(feature, result);
+    };
+    img.onerror = function () {
+        callback(feature, false);
+    };
+    img.src = "data:image/webp;base64," + kTestImages[feature];
+}
